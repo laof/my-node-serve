365cfe8ea0059a693fc0709dc97162ed8ad7c4db
laof <345603828@qq.com>
2017-08-31 00:00:42 +0800

    报文

--- serve/filter.js
+++ serve/filter.js
@@ -2,6 +2,7 @@ module.exports.login = (req, res, next) => {
     if (!req.session.userId) {
         
         if(req.headers['x-requested-with']==='XMLHttpRequest'){
+             res.setHeader('WWW-Authenticate', 'Basic realm="SECURE Area"');
              res.send({ redirect: true, code: 304 });
         }else{
             res.redirect('/login');
