e206607b3c9e8bf5c011c2788eeb402e4bafae69
laof <345603828@qq.com>
2017-09-15 08:23:11 +0800

    路由

--- serve/router.js
+++ serve/router.js
@@ -3,126 +3,133 @@ const qs = require('querystring');
 const url = require('url');
 const filter = require('./filter');
 const config = require('../config.json');
-const router = [
-    {
-        api: '/crawler',
-        http(req, res) {
 
-        }
-    },
-    {
-        api: '/outlogin',
-        http(req, res) {
-            req.session.userId = null;
-            req.session.username = null;
-            req.session.password = null;
+class CrwlerServe {
+    api = '/crawler';
+    serve(req, res) {
+
+    }
+}
+
+class OutloginServe {
+    api = '/outlogin';
+    serve(req, res) {
+        req.session.userId = null;
+        req.session.username = null;
+        req.session.password = null;
+        res.redirect('/home');
+    }
+}
+
+class DefaultServe {
+    api = '/*';
+    http(req, res) {
+        res.sendfile('dist/index.html');
+    }
+}
+
+class UserInfoServe {
+    api = '/userInfo';
+    http(req, res) {
+        res.send({
+            sucess: !0, user: {
+                id: req.session.userId,
+                name: req.session.username
+            }
+        });
+    }
+}
+
+class LoginServe {
+    api = '/login';
+    type = 'get';
+    http(req, res) {
+        if (req.session.userId) {
             res.redirect('/home');
+        } else {
+            res.sendfile('src/login.html');
         }
-    },
-    {
-        api: '/home',
-        type: 'get',
-        isLogin: true,
-        http(req, res) {
-            res.sendfile('src/home.html');
-        }
-    },
-    {
-        api: '/login',
-        type: 'get',
-        http(req, res) {
-            if (req.session.userId) {
-                res.redirect('/home');
-            } else {
-                res.sendfile('src/login.html');
+
+    }
+}
+
+class HomeServe {
+    api = '/home';
+    type = 'get';
+    isLogin = true;
+    http(req, res) {
+        res.sendfile('src/home.html');
+    }
+}
+
+class LoginServe {
+    api = '/user/login';
+    type = 'post';
+    http(req, res) {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        let success = false;
+        config.user.forEach((v, i) => {
+            if (username === v.name && password === v.password) {
+                success = true;
+                return false;
             }
+        })
 
+        if (success) {
+            req.session.userId = new Date().getTime();
+            req.session.username = req.body.username;
+            req.session.password = req.body.password;
+            res.redirect("/home.html");
+            return;
         }
-    },
-    {
-        api: '/userInfo',
-        http(req, res) {
-            res.send({
-                sucess: !0, user: {
-                    id: req.session.userId,
-                    name: req.session.username
-                }
-            });
-        }
-    },
-    {
-        api: '/user/login',
-        type: 'post',
-        http(req, res) {
-            const username = req.body.username;
-            const password = req.body.password;
+        res.send({ message: '用户名或者密码错误' });
 
-            let success = false;
-            config.user.forEach((v, i) => {
-                if (username === v.name && password === v.password) {
-                    success = true;
-                    return false;
-                }
-            })
+    }
+}
 
-            if (success) {
-                req.session.userId = new Date().getTime();
-                req.session.username = req.body.username;
-                req.session.password = req.body.password;
-                res.redirect("/home.html");
-                return;
-            }
-            res.send({ message: '用户名或者密码错误' });
+class WeatherServe {
+    isLogin = true;
+    api = '/weather/forecast';
+    http(req, res) {
 
-        }
-    },
-    {
-        isLogin: true,
-        api: '/weather/forecast',
-        http(req, res) {
+        const city = req.body.city;
+        //console.log(req.headers.cookie);
+        //console.log(arg.city);CHSC000000
 
-            const city = req.body.city;
-            //console.log(req.headers.cookie);
-            //console.log(arg.city);CHSC000000
+        const promise = new Promise((resolve, reject) => {
 
-            const promise = new Promise((resolve, reject) => {
-
-                http.get('http://tj.nineton.cn/Heart/index/all?city=' + city, (res) => {
-                    let data = '';
-                    const titles = [];
-                    res.setEncoding('utf-8');
-                    //监听data事件，每次取一块数据
-                    res.on('data', (chunk) => {
-                        data += chunk;
-                    });
-                    res.on('end', () => {
-                        resolve(data);
-                    });
-
-                }).on('error', (err) => {
-                    reject(err);
-                    console.log(err);
+            http.get('http://tj.nineton.cn/Heart/index/all?city=' + city, (res) => {
+                let data = '';
+                const titles = [];
+                res.setEncoding('utf-8');
+                //监听data事件，每次取一块数据
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    resolve(data);
                 });
 
+            }).on('error', (err) => {
+                reject(err);
+                console.log(err);
             });
 
-            promise.then((promres) => {
-                res.send(promres);
-            }, (err) => {
-                res.send({ sucess: false, code: 404 });
-            })
-        }
-    },
-    {
-        api: '/*',
-        http(req, res) {
-            res.sendfile('dist/index.html');
-        }
+        });
+
+        promise.then((promres) => {
+            res.send(promres);
+        }, (err) => {
+            res.send({ sucess: false, code: 404 });
+        })
     }
-]
+}
 
 exports.router = (app) => {
-    router.forEach((v, i) => {
+    console.log(this);
+    [].forEach((v, i) => {
         let type = 'post';
         if (Object.is(v.type, 'get')) {
             type = 'get';
